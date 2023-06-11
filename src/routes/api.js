1eced0078c3f0126fbdff8e38427761c5b06fff9
Ikhsans4 <m_ikhsan20@mhs.unsyiah.ac.id>
2023-06-11 11:47:21 +0700

    make connection sql

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
-// get the client
-const mysql = require('mysql2');
+const pool = require('../database/connection')
 
 // hash password
 const bcrypt = require('bcrypt');
@@ -11,19 +10,7 @@ const jwt = require('jsonwebtoken');
 const randomstring = require("randomstring");
 
 
-// Create the connection pool. The pool-specific settings are the defaults
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'herbalens',
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0
-});
+
 // const connection = mysql.createConnection({
 //     host: 'localhost',
 //     user: 'root',
