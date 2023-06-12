18b90e0593001dec43c400268d149ed63435feaa
Ikhsans4 <m_ikhsan20@mhs.unsyiah.ac.id>
2023-06-12 07:24:36 +0700

    feat:add .env & some configuration

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
 // get the client
 const mysql = require('mysql2');
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'herbalens',
+    host: dotenv.config().parsed.DB_HOST,
+    user: dotenv.config().parsed.DB_USER,
+    database: dotenv.config().parsed.DB_NAME,
+    password: dotenv.config().parsed.DB_PASS,
     waitForConnections: true,
     connectionLimit: 10,
     maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
