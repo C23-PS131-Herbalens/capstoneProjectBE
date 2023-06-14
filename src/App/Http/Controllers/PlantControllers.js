b76418d26ef08a36d91f5c32a5ccab42628a9561
daffa1134 <daffa1134@gmail.com>
2023-06-15 00:37:25 +0700

    Update query

--- src/App/Http/Controllers/PlantControllers.js
+++ src/App/Http/Controllers/PlantControllers.js
@@ -4,75 +4,89 @@ const plantController = {
     async getPlants(req, res) {
         let plantName = req.query.name;
         if (plantName) {
-            pool.query("SELECT p.plant_id, p.plant_name, p.image, p.description, p.benefit, t.* FROM plants p INNER JOIN taxonomy t ON p.plant_id = t.plant_id  WHERE p.plant_name = ? LIMIT 1", [plantName], (err, plants, fields) => {
-                if (err) {
-                    console.log(err);
+            pool.query(
+                "SELECT p.plant_id, p.plant_name, p.image, p.description, p.benefit, t.*, GROUP_CONCAT(ls.recipe SEPARATOR ';;') AS recipes FROM plants p INNER JOIN taxonomy t ON p.plant_id = t.plant_id INNER JOIN recipe_plants_list rp ON p.plant_id = rp.plant_id INNER JOIN recipe_list ls ON ls.recipe_id = rp.recipe_id  WHERE p.plant_name = ? GROUP BY p.plant_id LIMIT 1",
+                [plantName],
+                (err, plants, fields) => {
+                    if (err) {
+                        res.send(err);
+                    }
+                    const plantData = plants.map((item) => {
+                        let _taxonomy = {};
+                        _taxonomy.kingdom = item.kingdom;
+                        _taxonomy.division = item.division;
+                        _taxonomy.classis = item.classis;
+                        _taxonomy.ordo = item.ordo;
+                        _taxonomy.family = item.family;
+                        _taxonomy.genus = item.genus;
+                        _taxonomy.species = item.species;
+
+                        item.benefit = item.benefit.split(";;");
+                        item.recipes = item.recipes.split(";;");
+
+                        return { ...item, taxonomy: _taxonomy };
+                    });
+
+                    res.send(plantData);
                 }
-                const plantData = plants.map((item) => {
-                    let _taxonomy = {};
-                    _taxonomy.kingdom = item.kingdom;
-                    _taxonomy.division = item.division;
-                    _taxonomy.classis = item.classis;
-                    _taxonomy.ordo = item.ordo;
-                    _taxonomy.family = item.family;
-                    _taxonomy.genus = item.genus;
-                    _taxonomy.species = item.species;
-
-                    item.benefit = item.benefit.split(";;");
-
-                    return { ...item, taxonomy: _taxonomy };
-                });
-
-                res.send(plantData);
-            });
+            );
         } else {
-            pool.query("SELECT p.plant_id, p.plant_name, p.image, p.description, p.benefit, t.* FROM plants p INNER JOIN taxonomy t ON p.plant_id = t.plant_id", (err, plants, fields) => {
-                if (err) {
-                    console.log(err);
+            pool.query(
+                "SELECT p.plant_id, p.plant_name, p.image, p.description, p.benefit, t.*, GROUP_CONCAT(ls.recipe SEPARATOR ';;') AS recipes FROM plants p INNER JOIN taxonomy t ON p.plant_id = t.plant_id INNER JOIN recipe_plants_list rp ON p.plant_id = rp.plant_id INNER JOIN recipe_list ls ON ls.recipe_id = rp.recipe_id GROUP BY p.plant_id",
+                (err, plants, fields) => {
+                    if (err) {
+                        res.send(err);
+                    }
+
+                    const plantData = plants.map((item) => {
+                        let _taxonomy = {};
+                        _taxonomy.kingdom = item.kingdom;
+                        _taxonomy.division = item.division;
+                        _taxonomy.classis = item.classis;
+                        _taxonomy.ordo = item.ordo;
+                        _taxonomy.family = item.family;
+                        _taxonomy.genus = item.genus;
+                        _taxonomy.species = item.species;
+
+                        item.benefit = item.benefit.split(";;");
+                        item.recipes = item.recipes.split(";;");
+
+                        return { ...item, taxonomy: _taxonomy };
+                    });
+
+                    res.send(plantData);
                 }
-
-                const plantData = plants.map((item) => {
-                    let _taxonomy = {};
-                    _taxonomy.kingdom = item.kingdom;
-                    _taxonomy.division = item.division;
-                    _taxonomy.classis = item.classis;
-                    _taxonomy.ordo = item.ordo;
-                    _taxonomy.family = item.family;
-                    _taxonomy.genus = item.genus;
-                    _taxonomy.species = item.species;
-
-                    item.benefit = item.benefit.split(";;");
-
-                    return { ...item, taxonomy: _taxonomy };
-                });
-
-                res.send(plantData);
-            });
+            );
         }
     },
     async getPlantById(req, res) {
         let plantId = req.params.id;
-        pool.query("SELECT p.plant_id, p.plant_name, p.image, p.description, p.benefit, t.* FROM plants p INNER JOIN taxonomy t ON p.plant_id = t.plant_id WHERE p.plant_id = ? LIMIT 1", [plantId], (err, plants, fields) => {
-            if (err) {
-                console.log(err);
+        pool.query(
+            "SELECT p.plant_id, p.plant_name, p.image, p.description, p.benefit, t.*, GROUP_CONCAT(ls.recipe SEPARATOR ';;') AS recipes FROM plants p INNER JOIN taxonomy t ON p.plant_id = t.plant_id INNER JOIN recipe_plants_list rp ON p.plant_id = rp.plant_id INNER JOIN recipe_list ls ON ls.recipe_id = rp.recipe_id WHERE p.plant_id = ? GROUP BY p.plant_id LIMIT 1",
+            [plantId],
+            (err, plants, fields) => {
+                if (err) {
+                    res.send(err);
+                }
+                const plantData = plants.map((item) => {
+                    let _taxonomy = {};
+                    _taxonomy.kingdom = item.kingdom;
+                    _taxonomy.division = item.division;
+                    _taxonomy.classis = item.classis;
+                    _taxonomy.ordo = item.ordo;
+                    _taxonomy.family = item.family;
+                    _taxonomy.genus = item.genus;
+                    _taxonomy.species = item.species;
+
+                    item.benefit = item.benefit.split(";;");
+                    item.recipes = item.recipes.split(";;");
+
+                    return { ...item, taxonomy: _taxonomy };
+                });
+
+                res.send(plantData);
             }
-            const plantData = plants.map((item) => {
-                let _taxonomy = {};
-                _taxonomy.kingdom = item.kingdom;
-                _taxonomy.division = item.division;
-                _taxonomy.classis = item.classis;
-                _taxonomy.ordo = item.ordo;
-                _taxonomy.family = item.family;
-                _taxonomy.genus = item.genus;
-                _taxonomy.species = item.species;
-
-                item.benefit = item.benefit.split(";;");
-
-                return { ...item, taxonomy: _taxonomy };
-            });
-
-            res.send(plantData);
-        });
+        );
     },
 };
 
