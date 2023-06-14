09795ebfa60dd98b12560ccf7f02c8058054f5d7
daffa1134 <daffa1134@gmail.com>
2023-06-15 00:12:23 +0700

    Update query plants

--- src/App/Http/Controllers/PlantControllers.js
+++ src/App/Http/Controllers/PlantControllers.js
@@ -1,32 +1,79 @@
-const pool = require('../../../database/connection');
+const pool = require("../../../database/connection");
+
 const plantController = {
     async getPlants(req, res) {
-        let plantName = req.query.name
+        let plantName = req.query.name;
         if (plantName) {
-            pool.query("SELECT * FROM plants WHERE plant_name = ? LIMIT 1", [plantName], (err, plants, fields) => {
+            pool.query("SELECT p.plant_id, p.plant_name, p.image, p.description, p.benefit, t.* FROM plants p INNER JOIN taxonomy t ON p.plant_id = t.plant_id  WHERE p.plant_name = ? LIMIT 1", [plantName], (err, plants, fields) => {
                 if (err) {
                     console.log(err);
                 }
-                res.send({ plants });
+                const plantData = plants.map((item) => {
+                    let _taxonomy = {};
+                    _taxonomy.kingdom = item.kingdom;
+                    _taxonomy.division = item.division;
+                    _taxonomy.classis = item.classis;
+                    _taxonomy.ordo = item.ordo;
+                    _taxonomy.family = item.family;
+                    _taxonomy.genus = item.genus;
+                    _taxonomy.species = item.species;
+
+                    item.benefit = item.benefit.split(";;");
+
+                    return { ...item, taxonomy: _taxonomy };
+                });
+
+                res.send(plantData);
             });
         } else {
-            pool.query("SELECT * FROM plants", (err, plants, fields) => {
+            pool.query("SELECT p.plant_id, p.plant_name, p.image, p.description, p.benefit, t.* FROM plants p INNER JOIN taxonomy t ON p.plant_id = t.plant_id", (err, plants, fields) => {
                 if (err) {
                     console.log(err);
                 }
-                res.send({ plants });
-            })
+
+                const plantData = plants.map((item) => {
+                    let _taxonomy = {};
+                    _taxonomy.kingdom = item.kingdom;
+                    _taxonomy.division = item.division;
+                    _taxonomy.classis = item.classis;
+                    _taxonomy.ordo = item.ordo;
+                    _taxonomy.family = item.family;
+                    _taxonomy.genus = item.genus;
+                    _taxonomy.species = item.species;
+
+                    item.benefit = item.benefit.split(";;");
+
+                    return { ...item, taxonomy: _taxonomy };
+                });
+
+                res.send(plantData);
+            });
         }
     },
     async getPlantById(req, res) {
-        let plantId = req.params.id
-        pool.query("SELECT * FROM plants WHERE plant_id = ? LIMIT 1", [plantId], (err, plants, fields) => {
+        let plantId = req.params.id;
+        pool.query("SELECT p.plant_id, p.plant_name, p.image, p.description, p.benefit, t.* FROM plants p INNER JOIN taxonomy t ON p.plant_id = t.plant_id WHERE p.plant_id = ? LIMIT 1", [plantId], (err, plants, fields) => {
             if (err) {
                 console.log(err);
             }
-            res.send({ plants });
+            const plantData = plants.map((item) => {
+                let _taxonomy = {};
+                _taxonomy.kingdom = item.kingdom;
+                _taxonomy.division = item.division;
+                _taxonomy.classis = item.classis;
+                _taxonomy.ordo = item.ordo;
+                _taxonomy.family = item.family;
+                _taxonomy.genus = item.genus;
+                _taxonomy.species = item.species;
+
+                item.benefit = item.benefit.split(";;");
+
+                return { ...item, taxonomy: _taxonomy };
+            });
+
+            res.send(plantData);
         });
-    }
-}
+    },
+};
 
 module.exports = plantController;
